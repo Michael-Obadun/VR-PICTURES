b8b13e7e5ae25b2e61c2cad4f3838f8a64b6f743
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-23 15:03:03 +0000

    installed pillow and added it to requirmenrs.txt file. InhomePage/models created my first model and made migrations

--- homePage/models.py
+++ homePage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="blog_posts"
+)
+    image = models.ImageField(default='fallback.png', blank=False)
+    caption = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
