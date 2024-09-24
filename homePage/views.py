82e791181230b72506219c03dbaa01af2fd91293
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-24 13:14:39 +0000

    added link to stylesheet in base html

--- homePage/views.py
+++ homePage/views.py
@@ -6,5 +6,6 @@ from .models import Post
 class PostList(generic.ListView):
     #model = Post
     queryset = Post.objects.all()
-    template_name = "post_list.html"
+    template_name = "homePage/index.html"
+    paginate_by = 6
 
