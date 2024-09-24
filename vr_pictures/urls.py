8763db77411caa6fd369d0127c10f58db4da1ee8
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-24 04:29:50 +0000

    added summernote to vr_pictures/url file, url patterns and also imported it to homePage/admin file

--- vr_pictures/urls.py
+++ vr_pictures/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from homePage.views import my_homePage
 
 urlpatterns = [
     path('homePage/', my_homePage, name='homePage'),
+    path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
 ]
